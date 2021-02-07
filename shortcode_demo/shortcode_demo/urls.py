73ad2f93816215c03670e32809b75dc0e1909d9e
Brandon Lafving <blafving@gmail.com>
2021-02-07 20:17:00 +0000

    Deploy meetings page w sample data

--- shortcode_demo/shortcode_demo/urls.py
+++ shortcode_demo/shortcode_demo/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.http import HttpResponse
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -8,12 +9,15 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 
+with open("google_sheets_converted.json", "r") as file:
+    meetings_json = file.read()
+
 urlpatterns = [
     path('django-admin/', admin.site.urls),
 
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
-
+    path('meeting-json/', lambda request: HttpResponse(meetings_json), name='meeting-json'),
     path('search/', search_views.search, name='search'),
 
 ]
